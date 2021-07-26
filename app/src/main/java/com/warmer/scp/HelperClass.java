8668d13adbb79fa9aa267f0e4cfee12579d5e562
DIGILINE RANJAN <radhasonk.phy@gmail.com>
2021-07-26 17:56:15 +0530

    Commit

---
+++ app/src/main/java/com/warmer/scp/HelperClass.java
@@ -0,0 +1,15 @@
+package com.warmer.scp;
+
+public class HelperClass {
+    // Some other methods
+
+    public static int clamp( int min, int max, int value )
+    {
+        if( value > max )
+            return max;
+        else if( value < min )
+            return min;
+        else
+            return value;
+    }
+}
